61cd4cfcccdfaccd7be2f81e9dcc0397816ff440
jainiOfficial <jaini.official7668@gmail.com>
2026-08-23 19:29:14 +0530

    create one for field in auth context store to get user details in all screen throught useAuth function (#2)

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { useAuth } from '../context/AuthContext';
 
 export default function DashboardScreen() {
+
+  const { userDetails } = useAuth();
+  console.log('User details in DashboardScreen:', userDetails?.name || 'No user details available');
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Dashboard</Text>
+      <Text style={styles.text}>user: {userDetails?.name}</Text>
     </View>
   );
 }
