58caa79ea818beaec2478573e8c84d61cb31c184
Abhishek Jain <jaini.official7668@gmail.com>
2026-09-11 18:55:49 +0530

    full Finance module end to end completed hurraygit status

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import budgetReducer from './slices/budgetSlice';
+import financeReducer from './slices/financeSlice';
 
 export const store = configureStore({
-  reducer: { auth: authReducer, budget: budgetReducer }
-
+  reducer: { auth: authReducer, budget: budgetReducer, finance: financeReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
